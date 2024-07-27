9c15330b8b4ba030abaf9cdc81b0b18b981cdd9d
saku <ayana-03101@outlook.jp>
2024-07-27 18:25:55 +0900

    入力画面

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ConfirmationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [ConfirmationController::class, 'index']);
